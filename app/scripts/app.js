1f844670f7696d79d4b598244128086393bc870b
Sid Nutulapati <sidgonuts@gmail.com>
2015-06-24 10:57:57 -0400

    fix(Submit): Fields not clearing when submitted close #6

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name woaApp
+ * @description
+ * # woaApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('woaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ]);
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/display.html',
+        controller: 'viewPost'
+      })
+      .when('/submit', {
+        templateUrl: 'views/submit.html',
+        controller: 'enterPost'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+app.constant('FIREBASE_URL', 'https://incandescent-inferno-4169.firebaseio.com/');
+app.controller('viewPost', function ($scope, Post) {
+    $scope.derp = 'derp';
+    $scope.posts = Post.all;
+    $scope.deletePost = function (post) {
+        Post.delete(post);
+    };
+});
